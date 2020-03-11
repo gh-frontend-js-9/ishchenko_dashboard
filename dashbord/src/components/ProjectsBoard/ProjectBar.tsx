67dcbca1c255aade5f29df03e548452fa555f9aa
alex <appofeoz00@gmail.com>
2020-03-11 21:43:19 +0200

    "All projects" page in progress, needs fixes

--- dashbord/src/components/ProjectsBoard/ProjectBar.tsx
+++ dashbord/src/components/ProjectsBoard/ProjectBar.tsx
@@ -4,12 +4,23 @@ import ProgressBar from "../common/ProgressBar";
 
 function ProjectBar(props: any): any {
     let { project } = props;
+    /*let color;
+
+    if(project.progress == 0){
+        return color = '#fff'
+    }
+    if(project.progress >= 99){
+        return color = '#2e50a5'
+    }
+    else {
+        return color = '159E0A'
+    }*/
 
     return (
-        <div className={'flx-container'}>
-            <div>
+        <div className={'flx-container project-bar'}>
+            <div className={'flx-container__elem'}>
                 <span>{project.title}</span>
-                <span>{project.company}</span>
+                <span className={'name-block'}>{project.company}</span>
             </div>
             <div>
                 <span>{project.cost}</span>
@@ -18,18 +29,18 @@ function ProjectBar(props: any): any {
                 <span>{project.deadline}</span>
             </div>
             <div>
-                <span>{project.timeSpent}</span>
+                <span>{project.timeSpent} hours</span>
             </div>
             <div>
-                {/*<span>{project.progress}</span>
-                <progress max={100} value={project.progress} ></progress>*/}
+                <span>{project.progress}%</span>
+                <ProgressBar props={project}/>
             </div>
             <div>
                 <span>{project.status}</span>
             </div>
-            <div>
-                {/*<span>{project.assigned.phone}</span>
-                <span>{project.assigned.position}</span>*/}
+            <div className={'flx-container__elem'}>
+                <span>{project.assigned.name}</span>
+                <span className={'name-block'}>{project.assigned.position}</span>
             </div>
             <div>
                 <span>
--- dashbord/src/components/ProjectsBoard/ProjectsContainer.tsx
+++ dashbord/src/components/ProjectsBoard/ProjectsContainer.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ProjectBar from "./ProjectBar";
 import getAllProjects from "../../scripts/requests/getProjects";
+import './../../styles/style.css'
+import 'normalize.css'
 
 
 class ProjectsContainer extends React.Component<any, any>{
@@ -13,12 +15,20 @@ class ProjectsContainer extends React.Component<any, any>{
         getAllProjects().then((response: any) => {
             this.setState({projects: response.data})
         })
-
     }
 
     render(){
         return (
-            <div >
+            <div className={'projects-container'}>
+                <div className={'projects-container__panel'}>
+                    <span>Project title</span>
+                    <span>Value</span>
+                    <span>Deadline</span>
+                    <span>Time spent</span>
+                    <span>Progress</span>
+                    <span>Status</span>
+                    <span>Assigned to</span>
+                </div>
                 {this.state.projects.map((project: any) => <ProjectBar project={project} />)}
             </div>
         )
